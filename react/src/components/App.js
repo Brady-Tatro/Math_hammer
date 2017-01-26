2ffb4c39435a6be0ed1ad3c58051b205c0e85437
Brady Tatro <bradytatro@gmail.com>
2017-01-26 16:04:24 -0500

    added unit creation in react, will add db models to allow for testing... SHOULD GO BACK AND SPLIT UNIT COMPONENT

--- react/src/components/App.js
+++ react/src/components/App.js
@@ -4,14 +4,81 @@ class Unit extends Component {
   constructor() {
     super();
     this.state = {
+      army: '',
+      unitName: '',
+      minimumSize: '',
+      maximumSize: '',
+      ws: '',
+      bs: '',
+      strength: '',
+      toughness: '',
+      wounds: '',
+      iniative: '',
+      attacks: '',
+      leadership: '',
+      sv: '',
     };
     this.handleChange = this.handleChange.bind(this);
 
   };
+  handleChange(event) {
+    let nextState = {};
+    nextState[event.target.name] = event.target.value;
+    this.setState(nextState);
+  }
+
   render() {
     return (
       <div>
-      Hello World
+      Weapon Skill:<input
+        type="number"
+        value={this.ws}
+        name='ws'
+        onChange={this.handleChange}
+        />
+        Balistic Skill:<input
+          type="number"
+          value={this.bs}
+          name='bs'
+          onChange={this.handleChange}
+          />
+          Strength:<input
+            type="number"
+            value={this.strength}
+            name='strength'
+            onChange={this.handleChange}
+            />
+            Toughness:<input
+              type="number"
+              value={this.toughness}
+              name='toughness'
+              onChange={this.handleChange}
+              />
+              Wounds:<input
+                type="number"
+                value={this.wounds}
+                name='wounds'
+                onChange={this.handleChange}
+                />
+                Iniative:<input
+                  type="number"
+                  value={this.iniative}
+                  name='iniative'
+                  onChange={this.handleChange}
+                  />
+                  Leadership:<input
+                    type="number"
+                    value={this.leadership}
+                    name='leadership'
+                    onChange={this.handleChange}
+                    />
+                    Armor Save:<input
+                      type="number"
+                      value={this.sv}
+                      name='sv'
+                      onChange={this.handleChange}
+                      />
+
       </div>
     );
   };
