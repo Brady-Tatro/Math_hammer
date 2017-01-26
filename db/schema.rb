8d1bb736fbb616101d997b4d386d4b99651c883c
Brady Tatro <bradytatro@gmail.com>
2017-01-26 17:03:37 -0500

    added unit model, ajax needed

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20170126211419) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "units", force: :cascade do |t|
+    t.string  "army",        null: false
+    t.string  "unitName",    null: false
+    t.integer "minimumSize", null: false
+    t.integer "maximumSize", null: false
+    t.integer "ws",          null: false
+    t.integer "bs",          null: false
+    t.integer "strength",    null: false
+    t.integer "toughness",   null: false
+    t.integer "wounds",      null: false
+    t.integer "iniative",    null: false
+    t.integer "attacks",     null: false
+    t.integer "leadership",  null: false
+    t.integer "sv",          null: false
+  end
 
 end
